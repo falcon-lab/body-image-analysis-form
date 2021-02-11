6e2e77850b7df6b00bb4c61bb63f9d1d1b452736
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-10 22:55:56 -0300

    rearranged app to suit with tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,10 @@
 import React from 'react'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
 import type { AppProps } from 'next/app'
-
 import '../src/assets/styles/global.css';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: Archivo-Regular, Helvetica, sans-serif;
-  html, body {
-    min-height: 100vh;
-  }
-}
-`;
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <Component {...pageProps} />
   )
 }
